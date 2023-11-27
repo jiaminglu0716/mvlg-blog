93424ac16b9b39ed8f5c78a68cea4dc4a70e212f
jiaminglu0716 <1139473850@qq.com>
2023-11-27 19:34:52 +0900

    c d 2023-11-27 19:34:00 desc use tag plugin, note, to record some events what had happen.

--- shell/run.bat
+++ shell/run.bat
@@ -2,5 +2,7 @@ rem "hexo s ~ hexo server :: run hexo by local server"
 rem "hexo d ~ hexo deploy :: depploy hexo server"
 rem "hexo g ~ hexo generate :: generate static files"
 rem "run hexo with command as follow"
-hexo server
+@REM hexo server
+@REM Run server will display all draft posts, but hidden posts will stay at hidden status.
+hexo server --draft
 pause
---
+++ source/_posts/pin/Pin_Updated_Blog.md
@@ -0,0 +1,12 @@
+---
+title: 【Pin】Announcemation
+date: 9999-12-31 24:60:60
+---
+
+Updated
+
+### 11-27
+{% note %}
+- #### Function
+1. query some game information via the nav item of gamer. 
+{% endnote %}
