19642ef7b53658fb5800246785bc67f46e865f95
jiaminglu0716 <1139473850@qq.com>
2023-12-17 22:05:56 +0900

    t d 2023-12-17 22:05:00

--- themes/mvlg/scripts/plugins/vndb-plugin/index.js
+++ themes/mvlg/scripts/plugins/vndb-plugin/index.js
@@ -2,70 +2,83 @@ const { default: axios } = require("axios");
 const request = axios.request;
 
 
-const O = (path) => 'https://vndb.org' + path;
-const API = {
-  WEB: {
-    RANDOM: O('/v/rand'),
-    VN: (id) => O('/v/' + id)
+/**
+ * API DOC
+ * https://api.vndb.org/kana
+ */
+
+class VNDB {
+  static url = 'https://vndb.org';
+  static randlink = this.url + '/v/rand';
+  static vnlink(id) {
+    return this.url + '/v/' + id;
+  }
+
+  /**
+   * api-v2-kana
+   * @returns VNDBKana
+   */
+  static kana() {
+    return new VNDBKana();
+  }
+}
+
+class VNDBKana {
+  static api = 'https://api.vndb.org/kana';
+  static path(path) {
+    return this.api + path;
+  }
+  // 官方接口，但是随机不成功
+  async random() {
+    return request({
+      method: 'post',
+      url: 'https://api.vndb.org/kana/vn',
+      data: {
+        "sort": "id",
+        "reverse": true,
+        // "filters": ["and"
+        //   , [ "lang", "=", "ja" ]
+        //   , [ "id", ">=", "v1" ]
+        // ],
+        // "fields": "title",
+        "results": 1
+      }
+    });
+  }
+  // 官方Web随机重定向接口（仅单次发送）
+  // async randomV1() {
+  //   return request({
+  //     method: 'get',
+  //     url: API.WEB.RANDOM
+  //   }).then(res => res.request.res.responseUrl.replace('https://vndb.org/', ''));
+  // }
+  /**
+   * 内容接口
+   */
+  async vn(id) {
+    return request({
+      method: 'post',
+      url: VNDBKana.path('/vn'),
+      data: {
+        "filters": ["and"
+        , ["id","=",id]
+        , ["lang","=","ja"]],
+        "fields": "title, image.url, length_minutes, length_votes"
+      }
+    });
   }
 }
 
 
-// async function vn(id) {
-//   return request({
-//     method: 'post',
-//     url: 'https://api.vndb.org/kana/vn',
-//     data: {
-//       "filters": ["and"
-//       , ["id","=",id]
-//       , ["lang","=","ja"]],
-//       "fields": "title, image.url, length_minutes, length_votes"
-//     }
-//   }).then(res => res.data);
-// }
+class VNDBV2 extends VNDBKana {};
 
-// async function random() {
-//   return request({
-//     method: 'get',
-//     url: API.WEB.RANDOM
-//   }).then(res => res.request.res.responseUrl.replace('https://vndb.org/', ''));
-// }
+/**
+ * Test
+ */
+// VNDB.kana().vn(10087).then(res => res.data).then(console.log)
 
-
-// async function randomTimes(number, filters={}) {
-//   let urls = new Set();
-//   let results = new Array();
-
-//   for (let i = 0; i < number; i++) {
-//     urls.add(await random());
-//   }
-//   for (let url of urls.values()) {
-//     results.push(await vn(url));
-//   }
-//   return results;
-// }
-
-
-// randomTimes(1).then(r => console.log(r[0].results))
-
-
-
-
-function random() {
-  return request({
-    method: 'post',
-    url: 'https://api.vndb.org/kana/vn',
-    data: {
-      "sort": "id",
-      "reverse": true,
-
-      // "filters": ["and"
-      //   , [ "lang", "=", "ja" ]
-      //   , [ "id", ">=", "v1" ]
-      // ],
-      // "fields": "title",
-      "results": 1
-    }
-  }).then(r => console.log(r.data));
+module.exports = {
+  VNDB,
+  VNDBV2,
+  VNDBKana
 }
-random()
