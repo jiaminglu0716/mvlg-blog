5186162f7e899ff3a2edd29e0d610e460e2795a3
jiaminglu0716 <1139473850@qq.com>
2025-01-30 22:46:42 +0900

    fixed: eslint config to note

--- web/containers/web/cover/cover-mask.tsx
+++
@@ -1,7 +0,0 @@
-// import MaskImage from "../../../components/common/image/mask-image";
-
-// export default function CoverBackgroundContainer() {
-//   return (
-//     <MaskImage opacity="opacity-0" blur="none" src="./desk.jpg" alt="my desk" />
-//   );
-// }
